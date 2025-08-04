5e8590f10c5fb98896b28d9f1113d1fa2f742bea
Gabriel Vieira <nox030705@gmail.com>
2025-08-04 00:44:26 -0400

    added SPA architecture, create Tasks and Staff

---
+++ db/data.js
@@ -0,0 +1,53 @@
+// Staff definition
+/**
+ * @typedef {Object} Staff
+ * @property {string} name
+ * @property {string} nationalId
+ * @property {number} costPerHour
+ */
+
+
+/**
+ * @type {Staff[]}
+ * */
+export const staff = []
+
+// Materials definition
+/**
+ * @typedef {Object} Material
+ * @property {string} name
+ * @property {number} quantity
+ * @property {number} cost
+ */
+
+/**
+ * @type {Material[]}
+ */
+export const materials = []
+
+// Extra costs definition
+/**
+ * @typedef {Object} ExtraCost
+ * @property {number} quantity
+ * @property {number} cost
+ */
+
+/**
+ * @type {ExtraCost[]}
+ */
+export const extraCosts = []
+
+// Tasks definition
+/**
+ * @typedef {Object} Task
+ * @property {string} name
+ * @property {number} hours
+ * @property {Staff} staff
+ * @property {Material[]} materials
+ * @property {ExtraCost[]} extraCosts
+ */
+
+/**
+ * @type {Task[]}
+ */
+export const tasks = []
